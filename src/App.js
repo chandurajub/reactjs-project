7411101969a034dded5e754a239d0260f9d25c65
diensh <dinesh@gmail.com>
2022-04-25 23:25:18 +0530

    app.js file updated

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to the CI/CD for Reactjs.
         </p>
         <a
           className="App-link"
